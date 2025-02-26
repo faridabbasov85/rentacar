2d240dda8a5560913f939fc42b6c36b128a4b147
farid85 <faridra-sp102@code.edu.az>
2025-02-26 21:33:08 +0400

    Update thunk.js
    .

--- src/redux/thunks/products/thunk.js
+++ src/redux/thunks/products/thunk.js
@@ -2,16 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getProducts = createAsyncThunk("products/getProducts", async () => {
-    const res = await axios.get("http://localhost:5500/product")
+    const res = await axios.get("https://rentacar-r44c.vercel.app/product")
     return res.data
 })
 
 export const postProducts = createAsyncThunk("products/postProducts", async (data) => {
-    const res = await axios.post("http://localhost:5500/product", data)
+    const res = await axios.post("https://rentacar-r44c.vercel.app/product", data)
     return res.data
 })
 
 export const deleteProducts = createAsyncThunk("products/deleteProducts", async (_id) =>{
-    const res = await axios.delete("http://localhost:5500/product", {data: {_id}})
+    const res = await axios.delete("https://rentacar-r44c.vercel.app/product", {data: {_id}})
     return _id
 })
