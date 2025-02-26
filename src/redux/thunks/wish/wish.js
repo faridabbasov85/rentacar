032d6dc1fd21c8891abf40374d59eccd4b10f30d
farid85 <faridra-sp102@code.edu.az>
2025-02-26 21:33:49 +0400

    Update wish.js
    .

--- src/redux/thunks/wish/wish.js
+++ src/redux/thunks/wish/wish.js
@@ -2,16 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getWishs = createAsyncThunk("wishs/getWishs", async () => {
-    const res = await axios.get("http://localhost:5500/wishlist")
+    const res = await axios.get("https://rentacar-r44c.vercel.app/wishlist")
     return res.data
 })
 
 export const postWishs = createAsyncThunk("wishs/postWishs", async (data) => {
-    const res = await axios.post("http://localhost:5500/wishlist",data)
+    const res = await axios.post("https://rentacar-r44c.vercel.app/wishlist",data)
     return res.data
 })
 
 export const deleteWishs = createAsyncThunk("wishs/deleteWishs", async (_id) =>{
-    const res = await axios.delete("http://localhost:5500/wishlist", {data: {_id}})
+    const res = await axios.delete("https://rentacar-r44c.vercel.app/wishlist", {data: {_id}})
     return _id
 })
